8f3ded2e46aed391de1cbb071c35c5da30937c91
Miss-Ogallo <adhiambophoebe19@gmail.com>
2022-04-18 01:10:21 -0700

    modified

--- _printf.c
+++ _printf.c
@@ -1,47 +1,89 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * print_mod - will search for the required modifier function
+ * @format: the string we want manipulate
+ * @var: the list to search from
+ * Description: long description
+ *
+ * Return: the pointer to the string after the modification has occured
  */
+
+char *print_mod(char *format, va_list *var)
+{
+	unsigned int count = 0;
+	t_mod selector[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_int},
+		{"i", print_int},
+		{"b", print_binary},
+		{"o", print_eight},
+		{"x", print_sixteen},
+		{"X", print_u_sixteen},
+		{"u", print_uns},
+		{"p", print_ptr},
+		{"r", print_reverse},
+		{"R", print_rot},
+		{NULL, NULL}
+	};
+
+	if (*(format + 1) == '%')
+	{
+		remove_char(format);
+		return (format);
+	}
+
+	while (selector[count].symbol != NULL)
+	{
+		if (*(selector[count].symbol) == *(format + 1))
+		{
+			return (selector[count].type(format, var));
+		}
+		count++;
+	}
+	return (NULL);
+}
+
+/**
+ * _printf - will duplicate the printf function
+ * @format: the string we want manipulate
+ * Description: long description
+ *
+ * Return: count of the length printed to screen
+ */
+
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_list print, search;
+	unsigned int i = 0;
+	char *formatCpy;
 
-	register int count = 0;
+	va_start(print, format);
+	va_copy(search, print);
+	buffSize = buff_size_calc(format, &search);
+	va_end(search);
+	formatCpy = malloc(buffSize);
+	_strcpy(formatCpy, format);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	/**
+	 * These loops will find the modifier character and will
+	 * send to the print_mod function to do the heavy lifting.
+	 */
+	while (formatCpy != NULL && (*(formatCpy + i)) != '\0')
 	{
-		if (*p == '%')
+		if (*(formatCpy + i) == '%')
 		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			print_mod(formatCpy + i, &print);
+		}
+		i++;
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	va_end(print);
+	buffSize = _strlen(formatCpy);
+	write(1, formatCpy, buffSize);
+	free(formatCpy);
+	return (buffSize);
 }
